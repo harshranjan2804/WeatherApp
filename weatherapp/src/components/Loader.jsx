f056134b955f6d5fb4fc67c292d3e7d968aab098
harshranjan2804 <hranjan2804@gmail.com>
2025-04-09 07:34:25 +0530

    Loader Component Added

--- weatherapp/src/components/Loader.jsx
+++ weatherapp/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="flex justify-center items-center py-6">
-      <div className="w-10 h-10 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-10 h-10 border-4 border-red-400 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
