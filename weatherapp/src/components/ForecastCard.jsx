e39e4baff4eefb8531dba01094eeaa1c9af1ddf7
harshranjan2804 <hranjan2804@gmail.com>
2025-04-09 07:29:56 +0530

    ForecastCard Added

--- weatherapp/src/components/ForecastCard.jsx
+++ weatherapp/src/components/ForecastCard.jsx
@@ -5,7 +5,7 @@ const ForecastCard = ({ day, icon, temp, desc }) => {
     <div className="bg-white dark:bg-gray-700 text-center p-4 rounded-md shadow-sm text-sm text-gray-800 dark:text-white">
       <p className="font-semibold mb-1">{day}</p>
       <img src={icon} alt={desc} className="mx-auto w-12 h-12" />
-      <p className="text-lg font-bold">{temp}°C</p>
+      <p className="text-lg   font-bold">{temp}°C</p>
       <p className="capitalize mt-1">{desc}</p>
     </div>
   );
